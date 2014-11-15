9d55f71c934899f84f83f70f3b336575df57b803
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2014-11-15 10:20:31 -0200

    Initial tests with bulk insert operations in the MongoDB database.

--- src/mongodb/performance/MongoDBPerformance.java
+++ src/mongodb/performance/MongoDBPerformance.java
@@ -6,6 +6,21 @@
 
 package mongodb.performance;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.util.JSON;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author Pedro
@@ -15,8 +30,26 @@ public class MongoDBPerformance {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws UnknownHostException, FileNotFoundException, IOException {
+        MongoClient mongoClient = new MongoClient();
+        DB db = mongoClient.getDB("myDatabase");
+        
+        DBCollection collection = db.getCollection("ads");
+        collection.drop();
+        
+        BulkWriteOperation builder = collection.initializeUnorderedBulkOperation();
+        
+        FileInputStream fileInputStream = new FileInputStream(".\\resources\\MongoDB100000.txt");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+        
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            DBObject bson = (DBObject) JSON.parse(line);
+            builder.insert(bson);
+        }
+        bufferedReader.close();
+        BulkWriteResult result = builder.execute();
+        System.out.println("Resultado: " + result);
     }
     
 }
