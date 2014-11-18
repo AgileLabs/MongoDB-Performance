615f54daddf0223ff2b1bd9da33d27e21315cb2a
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2014-11-18 16:52:18 -0200

    Update and select operations show the total time. The generator now generates an Embedded Document (carroModelo) inside the ad document.

--- src/mongodb/generator/DataGenerator.java
+++ src/mongodb/generator/DataGenerator.java
@@ -30,6 +30,7 @@ public class DataGenerator {
     private static void createFile(int registers) {
         DataFileGenerator fileMongoDB = new DataFileGeneratorMongoDBImpl(registers);
 
+        String[] provinces = {"RS", "RJ", "SP", "SC", "PR", "MG", "MS", "MA", "PE", "CE", "BA"};
         String[] registrationPlates = {"ABC-1234", "AAA-1111", "ZZZ-9999"};
         int[] years = {2010, 2011, 2012, 2013, 2014, 2015};
         int[] models = {2010, 2011, 2012, 2013, 2014, 2015};
@@ -37,10 +38,21 @@ public class DataGenerator {
         String[] transmissions = {"Automático", "Manual"};
         String[] fuels = {"Gasolina", "Álcool", "Diesel"};
         String[] colors = {"Branco", "Prata", "Preto"};
+        String[] carModels = {", \"modeloCarro\": { \"marca\": \"RENAULT\", \"nome\": \"Clio RN 1.6 5p\", \"anoModelo\": 2002 } ",
+                             ", \"modeloCarro\": { \"marca\": \"RENAULT\", \"nome\": \"Megane Hatch RXE 2.0\", \"anoModelo\": 2012 } ",
+                             ", \"modeloCarro\": { \"marca\": \"RENAULT\", \"nome\": \"Twingo 1.0 8V\", \"anoModelo\": 2014 } ",
+                             ", \"modeloCarro\": { \"marca\": \"PEUGEOT\", \"nome\": \"206 Soleil 1.6 3p\", \"anoModelo\": 2006 } ",
+                             ", \"modeloCarro\": { \"marca\": \"PEUGEOT\", \"nome\": \"3008 Allure 1.6 Turbo 16V 5p Aut.\", \"anoModelo\": 2014 } ",
+                             ", \"modeloCarro\": { \"marca\": \"PEUGEOT\", \"nome\": \"307 Rallye 2.0 16V 138cv 5p Aut\", \"anoModelo\": 2015 } ",
+                             ", \"modeloCarro\": { \"marca\": \"VW\", \"nome\": \"Fox PRIME/Hghi. IMOTION 1.6 T.Flex 8V 5p\", \"anoModelo\": 2012 } ",
+                             ", \"modeloCarro\": { \"marca\": \"VW\", \"nome\": \"Gol 1.6 Mi Power Total Flex 8V 4p\", \"anoModelo\": 2011 } ",
+                             ", \"modeloCarro\": { \"marca\": \"VW\", \"nome\": \"Golf 1.6Mi/ 1.6Mi Gener./Black & Silver\", \"anoModelo\": 2009 } ",                             
+                             ", \"modeloCarro\": { \"marca\": \"VW\", \"nome\": \"Parati 2.0 Mi/ 2.0 Mi Track & Field\", \"anoModelo\": 2011 } "};
         String[] optionalNames = {"Vidro Elétrico", "Alarme", "DVD"};
         String[] texts = {"Bateria nova", "Inteiro", "Único dono"};
         String[] pathNames = {"\\anuncio\\fotos\\fotoABC.jpg", "\\anuncio\\fotos\\fotoDeFrente.jpg", "\\anuncio\\fotos\\fotoInterna.jpg"};
         
+        String province;
         String registrationPlate;
         int year;
         int model;
@@ -50,6 +62,7 @@ public class DataGenerator {
         String color;
         double kilometer;
         double value;
+        String carModel;
         String[] optionalName;
         String[] text;
         String[] pathName;
@@ -60,6 +73,7 @@ public class DataGenerator {
         StringBuilder register;
         
         for (int i = 0; i < registers; i++) {
+            province = provinces[(int) (Math.random() * provinces.length)];
             registrationPlate = registrationPlates[(int) (Math.random() * registrationPlates.length)];
             year = years[(int) (Math.random() * years.length)];
             model = models[(int) (Math.random() * models.length)];
@@ -69,6 +83,7 @@ public class DataGenerator {
             color = colors[(int) (Math.random() * colors.length)];
             kilometer = 1000 + (Math.random() * 100000);
             value = 5000 + (Math.random() * 100000);
+            carModel = carModels[(int) (Math.random() * carModels.length)];
             numberOptionalNames = (int) (Math.random() * 4);
             optionalName = new String[numberOptionalNames];
             numberTexts = (int) (Math.random() * 4);
@@ -87,7 +102,9 @@ public class DataGenerator {
             for (int j = 0; j < numberPathNames; j++) {
                 pathName[j] = pathNames[(int) (Math.random() * pathNames.length)];
             }
-            register = new StringBuilder("{ \"placa_carro\": \"");
+            register = new StringBuilder("{ \"uf\": \"");
+            register.append(province);
+            register.append("\", \"placa_carro\": \"");
             register.append(registrationPlate);
             register.append("\", \"ano\": ");
             register.append(year);
@@ -105,11 +122,12 @@ public class DataGenerator {
             register.append(kilometer);
             register.append(", \"valor\": ");
             register.append(value);
+            register.append(carModel);
                     
 
             for (int j = 0; j < numberOptionalNames; j++) {
                 if(j == 0){
-                    register.append(", opcional: ");
+                    register.append(", \"opcional\": ");
                     if(numberOptionalNames > 1){
                         register.append("[ ");
                     }
@@ -129,7 +147,7 @@ public class DataGenerator {
 
             for (int j = 0; j < numberTexts; j++) {
                 if(j == 0){
-                    register.append(", detalhe: ");
+                    register.append(", \"detalhe\": ");
                     if(numberTexts > 1){
                         register.append("[ ");
                     }
@@ -149,7 +167,7 @@ public class DataGenerator {
             
             for (int j = 0; j < numberPathNames; j++) {
                 if(j == 0){
-                    register.append(", foto: ");
+                    register.append(", \"foto\": ");
                     if(numberPathNames > 1){
                         register.append("[ ");
                     }
--- src/mongodb/performance/MongoDBPerformance.java
+++ src/mongodb/performance/MongoDBPerformance.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mongodb.performance;
 
 import com.mongodb.BasicDBObject;
@@ -11,6 +10,7 @@ import com.mongodb.BulkWriteOperation;
 import com.mongodb.BulkWriteResult;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.util.JSON;
@@ -31,35 +31,72 @@ public class MongoDBPerformance {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws UnknownHostException, FileNotFoundException, IOException {
-        if (args.length == 0){
-            System.out.println("Parâmetro não informado!");                            
+        if (args.length == 0) {
+            System.out.println("Parâmetro não informado!");
             System.exit(-1);
-        }    
-        System.out.println("Parâmetro: " + args[0]);       
+        }
+        System.out.println("Parâmetro: " + args[0]);
 
         MongoClient mongoClient = new MongoClient();
+        //MongoClient mongoClient = new MongoClient( "54.172.218.64" , 27017 );
         DB db = mongoClient.getDB("myDatabase");
-        
+
         DBCollection collection = db.getCollection("ads");
         collection.drop();
-        
+
         BulkWriteOperation builder = collection.initializeUnorderedBulkOperation();
-        
-        FileInputStream fileInputStream = new FileInputStream(".\\resources\\MongoDB100000.txt");
+
+        FileInputStream fileInputStream = new FileInputStream(".\\resources\\MongoDB" + args[0] + ".txt");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
         // Insert
         // Time start    
         long start = System.currentTimeMillis();
-        
+
         String line;
         while ((line = bufferedReader.readLine()) != null) {
             DBObject bson = (DBObject) JSON.parse(line);
             builder.insert(bson);
         }
         bufferedReader.close();
+        builder.execute();
         //Time end
         long elapsed = System.currentTimeMillis() - start;
         System.out.println("[insert] Time elapsed: " + elapsed + " ms");
+
+        // Update
+        // Time start    
+        start = System.currentTimeMillis();
+        collection.updateMulti(new BasicDBObject(), new BasicDBObject("$set", new BasicDBObject().append("ano", 2006)));
+        // Time end
+        elapsed = System.currentTimeMillis() - start;
+        System.out.println("[update] Time elapsed: " + elapsed + " ms");
+
+        // Select
+        // Time start    
+        start = System.currentTimeMillis();
+        BasicDBObject keys = new BasicDBObject();
+        keys.put("_id", 1);
+        keys.put("modeloCarro.marca", 1);
+        keys.put("modeloCarro.nome", 1);
+        keys.put("uf", 1);
+        keys.put("placa_carro", 1);
+        keys.put("qtd_portas", 1);
+        keys.put("cambio", 1);
+        keys.put("combustivel", 1);
+        keys.put("cor", 1);
+        keys.put("km", 1);
+        keys.put("valor", 1);
+        keys.put("detalhe", 1);
+        BasicDBObject sort = new BasicDBObject("_id", 1);
+
+        DBCursor cursor = collection.find(new BasicDBObject(), keys).sort(sort);
+        while (cursor.hasNext()) {
+            continue;
+        }
+        // Time end
+        elapsed = System.currentTimeMillis() - start;
+        System.out.println("[select] Time elapsed: " + elapsed + " ms");
+
     }
-    
+
 }
